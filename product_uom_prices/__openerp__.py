ee95f674a98dfde892158078fc11c9311ef0e5aa
Miguel Chuga <miguelchuga@gmail.com>
2015-04-26 21:10:55 -0600

    Balgo A

---
+++ product_uom_prices/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{'active': False,
+    'author': 'FARNET',
+    'category': 'base.module_category_knowledge_management',
+    'demo_xml': [],
+    'depends': [
+        'sale',
+        'product_price_currency',
+        ],
+    'description': """
+Product UOM Prices
+
+""",
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'Product UOM Prices',
+    'test': [],
+    'update_xml': [
+        'view/product_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'version': 'No version',
+    'website': ''}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
